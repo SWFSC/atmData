e909197c7281f0380b76a3749cd91cc45b757fc1
Kevin Stierhoff <kevins@ucsc.edu>
2023-03-01 15:49:59 -0800

    Updated scripts for importing data and defining regions. Updated abundance data.

--- data-raw/import-abundance.R
+++ data-raw/import-abundance.R
@@ -1,66 +1,94 @@
 ## code to prepare `abundance_*` data sets
 
+## Beginning in 2019, nearshore sampling began
+
 ## specify root directory
 estimATM.dir <- "C:/KLS/CODE/Github/estimATM"
 EstimateCPS.dir <- "C:/KLS/CODE/Github/EstimateCPS"
 
-## 2015 data
+## 2015 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "1507SH/Output/abundance_table_all.Rdata"))
-abundance_1507SH <- abund.summ
+abundance_1507SH <- dplyr::ungroup(abund.summ) %>%
+  dplyr::mutate(abund.summ, Region = "Core")
 
+## Save data and clean-up
 usethis::use_data(abundance_1507SH, overwrite = TRUE)
 rm(list = c("abund.summ", "abundance_1507SH"))
 
-## 2016 data
+## 2016 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "1606RL/Output/abundance_table_all.Rdata"))
-abundance_1606RL <- abund.summ
+abundance_1606RL <- dplyr::ungroup(abund.summ) %>%
+  dplyr::mutate(abund.summ, Region = "Core")
 
+## Save data and clean-up
 usethis::use_data(abundance_1606RL, overwrite = TRUE)
 rm(list = c("abund.summ", "abundance_1606RL"))
 
-## 2017 data
+## 2017 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(EstimateCPS.dir, "1707RL/Output/abundance_table_all.Rdata"))
-abundance_1707RL <- abund.summ
+abundance_1707RL <- dplyr::ungroup(abund.summ) %>%
+  dplyr::mutate(abund.summ, Region = "Core")
 
 usethis::use_data(abundance_1707RL, overwrite = TRUE)
 rm(list = c("abund.summ", "abundance_1707RL"))
 
-## 2018 data
+## 2018 data --------------------------------------
 load(file.path(EstimateCPS.dir, "1807RL/Output/abundance_table_all.Rdata"))
-abundance_1807RL <- abund.summ
+## Import data and define sampling regions
+abundance_1807RL <- dplyr::ungroup(abund.summ) %>%
+  dplyr::mutate(abund.summ, Region = "Core")
 
+## Save data and clean-up
 usethis::use_data(abundance_1807RL, overwrite = TRUE)
 rm(list = c("abund.summ", "abundance_1807RL"))
 
-## 2019 data
+## 2019 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "1907RL/Output/abundance_table_all.Rdata"))
 load(file.path(estimATM.dir, "1907RL/Output/abundance_table_all_ns.Rdata"))
-abund.summ    <- dplyr::mutate(abund.summ, Region = "Core") %>% dplyr::ungroup()
-abund.summ.ns <- dplyr::mutate(abund.summ.ns, Region = "Nearshore") %>% dplyr::ungroup()
+abund.summ    <-  dplyr::ungroup(abund.summ) %>%
+  dplyr::mutate(Region = "Core")
+abund.summ.ns <-  dplyr::ungroup(abund.summ.ns) %>%
+  dplyr::mutate(Region = "Nearshore")
 
+# Combine regions
 abundance_1907RL <- dplyr::bind_rows(abund.summ, abund.summ.ns)
 
+## Save data and clean-up
 usethis::use_data(abundance_1907RL, overwrite = TRUE)
 rm(list = c("abund.summ", "abundance_1907RL"))
 
-## 2021 data
+## 2021 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "2107RL/Output/abundance_table_all.Rdata"))
 load(file.path(estimATM.dir, "2107RL/Output/abundance_table_all_ns.Rdata"))
-abund.summ    <- dplyr::mutate(abund.summ, Region = "Core") %>% dplyr::ungroup()
-abund.summ.ns <- dplyr::mutate(abund.summ.ns, Region = "Nearshore") %>% dplyr::ungroup()
+abund.summ    <-  dplyr::ungroup(abund.summ) %>%
+  dplyr::mutate(Region = "Core")
+abund.summ.ns <-  dplyr::ungroup(abund.summ.ns) %>%
+  dplyr::mutate(Region = "Nearshore")
 
+# Combine regions
 abundance_2107RL <- dplyr::bind_rows(abund.summ, abund.summ.ns)
 
+## Save data and clean-up
 usethis::use_data(abundance_2107RL, overwrite = TRUE)
 rm(list = c("abund.summ", "abundance_2107RL"))
 
-## 2022 data
+## 2022 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "2207RL/Output/abundance_table_all.Rdata"))
 load(file.path(estimATM.dir, "2207RL/Output/abundance_table_all_ns.Rdata"))
-abund.summ    <- dplyr::mutate(abund.summ, Region = "Core") %>% dplyr::ungroup()
-abund.summ.ns <- dplyr::mutate(abund.summ.ns, Region = "Nearshore") %>% dplyr::ungroup()
+abund.summ    <-  dplyr::ungroup(abund.summ) %>%
+  dplyr::mutate(Region = "Core")
+abund.summ.ns <-  dplyr::ungroup(abund.summ.ns) %>%
+  dplyr::mutate(Region = "Nearshore")
 
+# Combine regions
 abundance_2207RL <- dplyr::bind_rows(abund.summ, abund.summ.ns)
 
+## Save data and clean-up
 usethis::use_data(abundance_2207RL, overwrite = TRUE)
 rm(list = c("abund.summ", "abundance_2207RL"))
--- data-raw/import-biomass.R
+++ data-raw/import-biomass.R
@@ -1,72 +1,93 @@
 ## code to prepare `biomass_*` data sets
 
-library(atmData)
-
 ## specify root directory
 estimATM.dir <- "C:/KLS/CODE/Github/estimATM"
 EstimateCPS.dir <- "C:/KLS/CODE/Github/EstimateCPS"
 
-## 2015 data
+## 2015 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "1507SH/Output/biomass_bootstrap_estimates_final.Rdata"))
 biomass_1507SH <- dplyr::ungroup(be) %>%
   dplyr::mutate(Region = "Core")
 
+## Save data and clean-up
 usethis::use_data(biomass_1507SH, overwrite = TRUE)
 rm(list = c("be", "biomass_1507SH"))
 
-## 2016 data
+## 2016 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "1606RL/Output/biomass_bootstrap_estimates_final.Rdata"))
 biomass_1606RL <- dplyr::ungroup(be) %>%
   dplyr::mutate(Region = "Core")
 
+## Save data and clean-up
 usethis::use_data(biomass_1606RL, overwrite = TRUE)
 rm(list = c("be", "biomass_1606RL"))
 
-## 2017 data
+## 2017 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(EstimateCPS.dir, "1707RL/Output/biomass_bootstrap_estimates_final.Rdata"))
 biomass_1707RL <- dplyr::ungroup(be) %>%
   dplyr::mutate(Region = "Core")
 
+## Save data and clean-up
 usethis::use_data(biomass_1707RL, overwrite = TRUE)
 rm(list = c("be", "biomass_1707RL"))
 
-## 2018 data
+## 2018 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(EstimateCPS.dir, "1807RL/Output/biomass_bootstrap_estimates_final.Rdata"))
 biomass_1807RL <- dplyr::ungroup(be) %>%
   dplyr::mutate(Region = "Core")
 
+## Save data and clean-up
 usethis::use_data(biomass_1807RL, overwrite = TRUE)
 rm(list = c("be", "biomass_1807RL"))
 
-## 2019 data
+## 2019 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "1907RL/Output/biomass_bootstrap_estimates_final.Rdata"))
 load(file.path(estimATM.dir, "1907RL/Output/biomass_bootstrap_estimates_final_ns.Rdata"))
-be    <- dplyr::mutate(be, Region = "Core") %>% dplyr::ungroup()
-be.ns <- dplyr::mutate(be.ns, Region = "Nearshore") %>% dplyr::ungroup()
+be    <- dplyr::ungroup(be) %>%
+  dplyr::mutate(Region = "Core")
+be.ns <- dplyr::ungroup(be.ns) %>%
+  dplyr::mutate(Region = "Nearshore")
 
+# Combine regions
 biomass_1907RL <- dplyr::bind_rows(be, be.ns)
 
+## Save data and clean-up
 usethis::use_data(biomass_1907RL, overwrite = TRUE)
 rm(list = c("be", "be.ns", "biomass_1907RL"))
 
-## 2021 data
+## 2021 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "2107RL/Output/biomass_bootstrap_estimates_final.Rdata"))
 load(file.path(estimATM.dir, "2107RL/Output/biomass_bootstrap_estimates_final_ns.Rdata"))
-be    <- dplyr::mutate(be, Region = "Core") %>% dplyr::ungroup()
-be.ns <- dplyr::mutate(be.ns, Region = "Nearshore") %>% dplyr::ungroup()
+be    <- dplyr::ungroup(be) %>%
+  dplyr::mutate(Region = "Core")
+be.ns <- dplyr::ungroup(be.ns) %>%
+  dplyr::mutate(Region = "Nearshore")
 
+# Combine regions
 biomass_2107RL <- dplyr::bind_rows(be, be.ns)
 
+## Save data and clean-up
 usethis::use_data(biomass_2107RL, overwrite = TRUE)
 rm(list = c("be", "be.ns", "biomass_2107RL"))
 
-## 2022 data
+## 2022 data --------------------------------------
+## Import data and define sampling regions
 load(file.path(estimATM.dir, "2207RL/Output/biomass_bootstrap_estimates_final.Rdata"))
 load(file.path(estimATM.dir, "2207RL/Output/biomass_bootstrap_estimates_final_ns.Rdata"))
-be    <- dplyr::mutate(be, Region = "Core") %>% dplyr::ungroup()
-be.ns <- dplyr::mutate(be.ns, Region = "Nearshore") %>% dplyr::ungroup()
+be    <- dplyr::ungroup(be) %>%
+  dplyr::mutate(Region = "Core")
+be.ns <- dplyr::ungroup(be.ns) %>%
+  dplyr::mutate(Region = "Nearshore")
 
+# Combine regions
 biomass_2207RL <- dplyr::bind_rows(be, be.ns)
 
+## Save data and clean-up
 usethis::use_data(biomass_2207RL, overwrite = TRUE)
 rm(list = c("be", "be.ns", "biomass_2207RL"))
