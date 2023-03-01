75dc8ba06d8ec9dcc8332e73fd461831c4be60e7
Kevin Stierhoff <kevins@ucsc.edu>
2023-03-01 12:49:19 -0800

    Added remaining nasc_density data. Updated DESCRIPTION and README.

--- data-raw/import-nasc-density.R
+++ data-raw/import-nasc-density.R
@@ -9,9 +9,48 @@ load(file.path(estimATM.dir, "1507SH/Output/nasc_final.Rdata"))
 nasc_density_1507SH <- nasc
 
 usethis::use_data(nasc_density_1507SH, overwrite = TRUE)
+rm(list = c("nasc", "nasc_density_1507SH"))
 
 ## 2016 data
 load(file.path(estimATM.dir, "1606RL/Output/nasc_final.Rdata"))
 nasc_density_1606RL <- nasc
 
 usethis::use_data(nasc_density_1606RL, overwrite = TRUE)
+rm(list = c("nasc", "nasc_density_1606RL"))
+
+## 2017 data
+load(file.path(EstimateCPS.dir, "1707RL/Output/nasc_final.Rdata"))
+nasc_density_1707RL <- nasc
+
+usethis::use_data(nasc_density_1707RL, overwrite = TRUE)
+rm(list = c("nasc", "nasc_density_1707RL"))
+
+## 2018 data
+load(file.path(estimATM.dir, "1807RL/Output/nasc_final.Rdata"))
+nasc_density_1807RL <- nasc
+
+usethis::use_data(nasc_density_1807RL, overwrite = TRUE)
+rm(list = c("nasc", "nasc_density_1807RL"))
+
+## 2019 data
+load(file.path(estimATM.dir, "1907RL/Output/nasc_final.Rdata"))
+nasc_density_1907RL <- nasc
+
+usethis::use_data(nasc_density_1907RL, overwrite = TRUE)
+rm(list = c("nasc", "nasc_density_1907RL"))
+
+## 2021 data
+load(file.path(estimATM.dir, "2107RL/Output/nasc_final.Rdata"))
+nasc_density_2107RL <- nasc
+
+usethis::use_data(nasc_density_2107RL, overwrite = TRUE)
+rm(list = c("nasc", "nasc_density_2107RL"))
+
+## 2022 data
+load(file.path(estimATM.dir, "2207RL/Output/nasc_final.Rdata"))
+nasc_density_2207RL <- nasc
+
+usethis::use_data(nasc_density_2207RL, overwrite = TRUE)
+rm(list = c("nasc", "nasc_density_2207RL"))
+
+
